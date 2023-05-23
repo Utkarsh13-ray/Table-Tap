c852a5496444844c102bb94cc6e8d6e31631c2fe
harshitg1 <harshit31.ugcs20@iiitranchi.ac.in>
2023-05-23 17:29:43 +0530

    Updated in Menu

---
+++ pages/Kl.js
@@ -0,0 +1,15 @@
+import Modal from '@/component/Modal'
+import React from 'react'
+
+const Kl = () => {
+  return (
+    <>
+    <div className="App h-screen flex flex-col items-center justify-center bg-purple-200">
+      <Modal />
+    </div>
+    
+    </>
+  )
+}
+
+export default Kl
