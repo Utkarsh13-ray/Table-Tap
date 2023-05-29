ee5fb983a2c6a3daaefc1babe1e9b516a5f3442b
Utkarsh <utkarsh20.ugcs32@iiitranchi.ac.in>
2023-05-29 17:54:26 +0530

    Landing  & Thankyou Page Updated

---
+++ pages/Thank.js
@@ -0,0 +1,12 @@
+import React from 'react'
+
+export default function Thank() {
+  return (
+    <div className="flex flex-col h-full  justify-center items-center w-full ">
+    <h1 className="  mx-auto font-bold text-6xl my-10">Thank you for Ordering!</h1>
+    <div>
+       <img src=" tick.png" className='animate-bounce mt-14' alt="" />
+    </div>
+</div>
+  )
+}
